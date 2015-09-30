1cd291bcc913f1cb79f6f95068e6cc7ac764b410
Peter Cageao <peter.cageao@gmail.com>
2015-09-30 15:22:14 -0400

    adds post.js

--- public_html/app/scripts/services/post.js
+++ public_html/app/scripts/services/post.js
@@ -1,5 +1,6 @@
 'use strict';
 
-app.factory('Post', function($resource){
-    return $resource('https://bottombitches.firebaseio.com/posts/:id.json');
-})
+app.factory('Post', function($firebase, FIREBASE_URL){
+    var ref = new Firebase(FIREBASE_URL);
+    var posts = $firebase(ref.child('posts').$asArray());
+});
