e2a7702bc6c7e02fbc5bf5fbd2fb113625f18a04
Peter Cageao <peter.cageao@gmail.com>
2015-09-30 14:52:20 -0400

    initial commit

---
+++ public_html/app/scripts/app.js
@@ -0,0 +1,32 @@
+/* global app: true */
+/* exported app */
+
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name publicHtmlApp
+ * @description
+ * # publicHtmlApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('publicHtmlApp', [ 'ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ngTouch' ]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl',
+        controllerAs: 'posts'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ public_html/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name publicHtmlApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the publicHtmlApp
+ */
+angular.module('publicHtmlApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
