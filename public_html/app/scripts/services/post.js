2e04d46cf082501658057e0e3ca74f61d9882da4
Peter Cageao <peter.cageao@gmail.com>
2015-09-30 15:29:38 -0400

    more modifications to post.js

--- public_html/app/scripts/services/post.js
+++ public_html/app/scripts/services/post.js
@@ -3,4 +3,21 @@
 app.factory('Post', function($firebase, FIREBASE_URL){
     var ref = new Firebase(FIREBASE_URL);
     var posts = $firebase(ref.child('posts').$asArray());
+
+    var Post = {
+        all: posts,
+        create: function(post){
+            return posts.add(post);
+        },
+
+        get: function(postId) {
+            return $firebase(ref.child('posts').child(postId)).$asObject();
+        },
+
+        delete: function(post){
+            return post.$remove(post);
+        }
+    };
+
+    return Post;
 });
