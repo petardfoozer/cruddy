70fa1d269a418a72ce031d178b9f08160cb2d514
Peter Cageao <peter.cageao@gmail.com>
2015-09-30 15:33:00 -0400

    changes PostCtrl to work with firebase methods

--- public_html/app/scripts/controllers/posts.js
+++ public_html/app/scripts/controllers/posts.js
@@ -1,12 +1,12 @@
 'use strict';
 
 app.controller('PostsCtrl', function ($scope, Post) {
-    $scope.posts = [];
+    $scope.posts = Post.all;
     $scope.post = {url: 'http://', title: ''};
 
     $scope.submitPost = function ()
     {
-        Post.save($scope.post, function(ref) {
+        Post.create($scope.post.then(function() {
             $scope.posts[ref.name] = $scope.post;
             $scope.post = {url: 'http://', title: ''};
         });
@@ -14,9 +14,7 @@ app.controller('PostsCtrl', function ($scope, Post) {
 
     $scope.deletePost = function(postId)
     {
-        Post.delete({id: postId}, function(){
-            delete $scope.posts[postId];
-        });
+        Post.delete(post);
     };
 
 });
