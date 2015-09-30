649f12006d6c1b4be8ca2fb5bfe2c12219192a21
Peter Cageao <peter.cageao@gmail.com>
2015-09-30 15:44:29 -0400

    adds a button to get all posts possibly

--- public_html/app/scripts/controllers/posts.js
+++ public_html/app/scripts/controllers/posts.js
@@ -4,6 +4,22 @@ app.controller('PostsCtrl', function ($scope, Post) {
     $scope.posts = Post.all;
     $scope.post = {url: 'http://', title: ''};
 
+    $scope.existingPosts = [];
+    $scope.getAllPosts = function ()
+    {
+        $http.get('https://bottombitches.firebaseio.com/posts/')
+        .success(function(data, status, headers)
+        {
+            $scope.existingPosts = data;
+            console.log($scope.existingPosts);
+        })
+        .error(function(data, status, headers, config)
+        {
+            console.log("There was an error loading the posts")
+        });
+
+    };
+
     $scope.submitPost = function ()
     {
         Post.create($scope.post).then(function() {
